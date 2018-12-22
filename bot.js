f4b85cca47f2542f47504cf387253db96a3d4142
esseli1 <46043777+esseli1@users.noreply.github.com>
2018-12-22 15:24:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "$";
+const prefix = ".";
 
 // ========================================== [ CONSTRUCTERS ] =========================================
 
@@ -173,4 +173,4 @@ client.on("message", async message => {
 // CODES - COPYRIGHT
 
 
-client.login("NTA5NDQ4NTg2MzEyOTQxNTg5.DuNNow.vJW3YAdlYJNbPTVTX3DqVOPCNqY");
+client.login("process.env.BOT_TOKEN");
